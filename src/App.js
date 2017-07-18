dd7eaed2d04599d7f3adc651c9c76425eb80b06b
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-07-18 10:10:10 -0500

    get app code updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import Nav from './components/Nav/Nav'
+
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            <Nav />
+            <div className="profile-container">
+               <div className="profile-image">
+                  <img src="https://image.flaticon.com/icons/svg/16/16480.svg" alt=""/>
+               </div>
+               <div className="profile-info">
+                  <h2>name</h2>
+                  <h3 className="profile-info-status">status</h3>
+                  <h4 className="profile-info-birthdate">birthdate</h4>
+               </div>
+            </div>
+
+            <form className="profile-editor" onSubmit={this.updateProfile}>
+               <input type="text" className="profile-editor-input"
+                  placeholder="Enter new name"
+               />
+               <input type="text" className="profile-editor-input"
+                  placeholder="Enter new image"
+               />
+               <input type="text" className="profile-editor-input"
+                  placeholder="Enter new birthdate"
+               />
+               <input type="text" className="profile-editor-input"
+                  placeholder="Enter new status"
+               />
+               <input className="profile-editor-submit" type="submit" value="Update Profile"/>
+
+            </form>
+         </div>
+      );
+   }
 }
 
 export default App;
